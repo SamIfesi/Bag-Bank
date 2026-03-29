3b11c7b9e4b717a8148fb5d71bd9ec232b6ab377
SamIfesi <Ekesamrose@gmail.com>
2026-03-29 20:51:03 +0100

    Adds inactivity timeout notice on login
    Displays a clear sign-in message after automatic logout due to inactivity, so users understand why they were redirected and can re-authenticate without confusion.
    
    Also tidies markup formatting to improve readability and maintainability.

--- views/login.php
+++ views/login.php
@@ -9,64 +9,71 @@ unset($_SESSION['errors']);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="../public/favicon.svg" type="image/svg" />
-    <link rel="stylesheet" href="../public/assets/css/style.css">
-    <title>D'Bag Bank - Login</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="../public/favicon.svg" type="image/svg" />
+  <link rel="stylesheet" href="../public/assets/css/style.css">
+  <title>D'Bag Bank - Login</title>
 </head>
 
 <body class="body">
-    <main>
-        <img src="../public/logo-stacked.svg" alt="" class="logo" />
-        <div class="loader-container hide" id="loader">
-            <div class="spinner"></div>
+  <main>
+    <img src="../public/logo-stacked.svg" alt="" class="logo" />
+    <div class="loader-container hide" id="loader">
+      <div class="spinner"></div>
+    </div>
+
+    <i class="ti ti-chevron-left back" onclick="window.location.href='../index.php'"></i>
+
+    <?php if (isset($_GET['timeout']) && $_GET['timeout'] === '1'): ?>
+      <p class="error showMsg" style="text-align:center; margin-bottom: 1rem;">
+        You were logged out after 10 minutes of inactivity. Please sign in again.
+      </p>
+    <?php endif; ?>
+    
+    <form action="../app/handlers/process_login.php" method="post" id="login-form">
+      <div id="userLogin-form">
+        <div class="form-group">
+          <i class="ti ti-user icon"></i>
+          <input
+            type="text"
+            class="input-form"
+            id="username"
+            name="username"
+            placeholder="Enter username" />
         </div>
+        <span class="error showMsg" id="user-error">
+          <?php foreach ($errors as $error): ?>
+            <?php echo htmlspecialchars($error); ?>
+          <?php endforeach; ?>
+        </span>
+        <button type="submit" class="btn-submit continue" id="continueLogin">Continue</button>
+        <button type="button" id="register" class="btn-submit" onclick="window.location.href='register.php'">Register</button>
+      </div>
 
-        <i class="ti ti-chevron-left back" onclick="window.location.href='../index.php'"></i>
-        <form action="../app/handlers/process_login.php" method="post" id="login-form">
-            <div id="userLogin-form">
-                <div class="form-group">
-                    <i class="ti ti-user icon"></i>
-                    <input
-                        type="text"
-                        class="input-form"
-                        id="username"
-                        name="username"
-                        placeholder="Enter username" />
-                </div>
-                <span class="error showMsg" id="user-error">
-                    <?php foreach ($errors as $error): ?>
-                        <?php echo htmlspecialchars($error); ?>
-                    <?php endforeach; ?>
-                </span>
-                <button type="submit" class="btn-submit continue" id="continueLogin">Continue</button>
-                <button type="button" id="register" class="btn-submit" onclick="window.location.href='register.php'">Register</button>
-            </div>
+      <!-- Password div -->
+      <div id="pswdLogin-form" class="hide">
+        <i class="ti ti-chevron-left back" id="backToLogin"></i>
+        <div class="form-group">
+          <i class="ti ti-eye icon" id="psdShow"></i>
+          <input
+            type="password"
+            class="input-form"
+            id="password"
+            name="password"
+            placeholder="Enter password" />
+        </div>
+        <span class="error" id="password-error"></span>
+        <button type="submit" class="btn-submit">Login</button>
+        <p class="alt-login">
+          Don't have an account? <a href="register.php">Register here</a>
+        </p>
+      </div>
 
-            <!-- Password div -->
-            <div id="pswdLogin-form" class="hide">
-                <i class="ti ti-chevron-left back" id="backToLogin"></i>
-                <div class="form-group">
-                    <i class="ti ti-eye icon" id="psdShow"></i>
-                    <input
-                        type="password"
-                        class="input-form"
-                        id="password"
-                        name="password"
-                        placeholder="Enter password" />
-                </div>
-                <span class="error" id="password-error"></span>
-                <button type="submit" class="btn-submit">Login</button>
-                <p class="alt-login">
-                    Don't have an account? <a href="register.php">Register here</a>
-                </p>
-            </div>
-
-        </form>
-    </main>
-    <span class="msg-success" id="msg-success"></span>
-    <script type="module" src="../public/assets/js/main.js"></script>
+    </form>
+  </main>
+  <span class="msg-success" id="msg-success"></span>
+  <script type="module" src="../public/assets/js/main.js"></script>
 </body>
 
 </html>
